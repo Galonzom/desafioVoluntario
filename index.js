85f47b95adc6059afad32f3555394bc6cfd780d1
Galonzom <86978716+Galonzom@users.noreply.github.com>
2022-08-17 14:48:21 -0400

    desafio voluntario

---
+++ index.js
@@ -0,0 +1,7 @@
+let nombre = prompt('Ingrese su nombre: ');
+let signo = prompt('Ingrese su signo: ');
+let mesNacimiento = prompt('Ingrese su mes de nacimiento')
+
+document.getElementById("nombre").innerHTML = nombre; 
+document.getElementById("signo").innerHTML = signo; 
+document.getElementById("nacimiento").innerHTML = mesNacimiento; 
